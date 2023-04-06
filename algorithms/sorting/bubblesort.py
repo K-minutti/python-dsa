5268508b9bbe33da0a66ff252acf218b01688426
Kevin Minutti <minutti70@gmail.com>
2023-04-06 18:08:54 -0400

    Fix bubble sort

--- algorithms/sorting/bubblesort.py
+++ algorithms/sorting/bubblesort.py
@@ -1,13 +1,8 @@
 
 def sort(nums: list[int]) -> list[int]:
-    n = len(nums)-1
-    if n < 1:
-        return nums
-    
-    while n >= 0:
+    for c in range(len(nums)):
         i=1
-        for i in range(i, n):
+        for i in range(i, (len(nums)-c)):
             if nums[i-1] > nums[i]:
                 nums[i-1] , nums[i] = nums[i], nums[i-1]
-        n-=1
     return nums
