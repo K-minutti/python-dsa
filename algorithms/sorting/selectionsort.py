095e65418b12f032dec8844e9e74866154b9f4a0
Kevin Minutti <minutti70@gmail.com>
2023-04-09 07:56:57 -0400

    Add selection sort

--- algorithms/sorting/selectionsort.py
+++ algorithms/sorting/selectionsort.py
@@ -1,3 +1,10 @@
 
 def sort(nums: list[int]) -> list[int]:
+    for i in range(len(nums)):
+        min_idx = i
+        for j in range(i, len(nums)):
+            if nums[j] < nums[min_idx]:
+                min_idx = j
+        if min_idx != i:
+            nums[i], nums[min_idx] = nums[min_idx], nums[i]
     return nums
--- algorithms/sorting/test_sort.py
+++ algorithms/sorting/test_sort.py
@@ -2,6 +2,7 @@ import pytest
 import quicksort
 import bubblesort
 import insertionsort
+import selectionsort
 
 def test_quicksort_n():
     a = [4, 3, 6, 7, 1]
@@ -18,6 +19,12 @@ def test_bubblesort_n():
 def test_insertionsort_n():
     a = [4, 3, 6, 7, 1]
     insertionsort.sort(a)
+    expected = [1, 3, 4, 6, 7]
+    assert a == expected
+
+def test_selectionsort_n():
+    a = [4, 3, 6, 7, 1]
+    selectionsort.sort(a)
     expected = [1, 3, 4, 6, 7]
     assert a == expected
 
