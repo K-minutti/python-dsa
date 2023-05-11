627d87445ff7bcdbe31cfdf41f99b9791d276145
Kevin Minutti <minutti70@gmail.com>
2023-05-10 23:35:12 -0400

    Add initial stack

--- data_structures/non_primiptives/linear/stack.py
+++ data_structures/non_primiptives/linear/stack.py
@@ -1,4 +1,28 @@
+'''
+In python a stack can be easily implemented with a list
+Operations
+add
+pop
+
+
+'''
+
+
 
 class Stack:
+ 
     def __init__(self):
-        pass
+        self.stack = []
+
+    def add(self, val: int):
+        self.append(val)
+
+    def pop(self) -> int:
+        return self.pop()
+
+    def contains(self, val) -> bool:
+        return val in self.stack
+
+
+
+
