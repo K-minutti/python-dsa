c5d16e5cac4938bfe602d2a8b8e8c1d83b0d2228
Kevin Minutti <minutti70@gmail.com>
2023-05-18 23:21:27 -0400

    Add queue push/pop method

--- data_structures/non_primiptives/linear/queue.py
+++ data_structures/non_primiptives/linear/queue.py
@@ -5,4 +5,21 @@ Queue
 class Queue: 
 
     def __init__(self):
-        pass
+        self.queue = []
+    
+    def push(self, val: int):
+        self.queue.append(val)
+    
+    def pop(self) -> int:
+        assert len(self.queue) > 0, "Cannot call pop() on empty queue"
+        val = self.queue[0]
+        self.queue = self.queue[1:]
+        return val
+
+
+if __name__ == "__main__":
+    s = Queue()
+    s.push(1)
+    s.push(2)
+    s.push(3)
+    print(s.pop())
