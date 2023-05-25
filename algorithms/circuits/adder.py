dce014c91a784beb77285ce166377ec688ac1f2c
Kevin Minutti <minutti70@gmail.com>
2023-05-24 22:34:09 -0400

    Add half adder

--- algorithms/circuits/adder.py
+++ algorithms/circuits/adder.py
@@ -1,9 +1,26 @@
 '''
 Half and full adder
 '''
+from typing import Tuple
 
-def half_adder(a: int, b: int):
-    pass
+def half_adder(a: int, b: int) -> Tuple[int, int]:
+    '''Truth table, a, b, sum, carry
+    a |  b  |  s  |  c
+    --------------------
+    0 |  0  |  0  |  0
+    0 |  1  |  1  |  0
+    1 |  0  |  1  |  0
+    1 |  1  |  0  |  1
+    The sum of the truth table can be found
+    with XOR and the carry can be found with 
+    the AND op
+    '''
+    s = a ^ b
+    c = a & b
+    return s, c
+    
 
-def full_adder(a: int, b: int):
-    pass
+def full_adder(a: int, b: int, c: int) -> Tuple[int, int]:
+    '''
+    A full adder from two half adders
+    '''
---
+++ algorithms/circuits/test_adders.py
@@ -0,0 +1,11 @@
+import adder
+
+def test_half_adder():
+    s, c = adder.half_adder(1, 1)
+    s_1, c_1 = adder.half_adder(1, 0)
+    s_2, c_2 = adder.half_adder(0, 1)
+    s_3, c_3 = adder.half_adder(0, 0)
+    assert s == 0 and c ==1
+    assert s_1 == 1 and c_1 == 0 
+    assert s_2 == 1 and c_2 == 0
+    assert s_3 == 0 and c_3 == 0
