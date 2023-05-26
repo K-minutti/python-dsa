ef64ff195c7ee0c1ecdaa878f3cb560c43323882
Kevin Minutti <minutti70@gmail.com>
2023-05-26 00:55:25 -0400

    Add full adder

--- algorithms/circuits/adder.py
+++ algorithms/circuits/adder.py
@@ -22,5 +22,27 @@ def half_adder(a: int, b: int) -> Tuple[int, int]:
 
 def full_adder(a: int, b: int, c: int) -> Tuple[int, int]:
     '''
-    A full adder from two half adders
+    We can take the truth table from the 
+    half adder and extend it by considering 
+    the additional carry input to get the two 
+    outputs sum, and carry required of a full adder
+    a |  b  |  c  |  s  | c_2
+    --------------------------
+    0 |  0  |  0  |  0  |  0
+    0 |  1  |  0  |  1  |  0
+    1 |  0  |  0  |  1  |  0
+    1 |  1  |  0  |  0  |  1
+    0 |  0  |  1  |  1  |  0
+    0 |  1  |  1  |  0  |  1
+    1 |  0  |  1  |  0  |  1
+    1 |  1  |  1  |  1  |  1
+    
+    
     '''
+    s = a ^ b ^ c # xor
+    pre_c1 = a & b
+    pre_c2 = b & c
+    pre_c3 = a & c
+    c2 = pre_c1 | pre_c2 | pre_c3
+    return s , c2
+
--- algorithms/circuits/test_adders.py
+++ algorithms/circuits/test_adders.py
@@ -9,3 +9,19 @@ def test_half_adder():
     assert s_1 == 1 and c_1 == 0 
     assert s_2 == 1 and c_2 == 0
     assert s_3 == 0 and c_3 == 0
+
+
+def test_full_adder():
+    s, c = adder.full_adder(0,0,0)
+    s_1, c_1 = adder.full_adder(0,1,0)
+    s_2, c_2 = adder.full_adder(1,0,0)
+    s_3, c_3 = adder.full_adder(1,1,0)
+    s_4, c_4 = adder.full_adder(0,0,1)
+    s_5, c_5 = adder.full_adder(0,1,1)
+    
+    assert s == 0 and c == 0
+    assert s_1 == 1 and c_1 == 0 
+    assert s_2 == 1 and c_2 == 0
+    assert s_3 == 0 and c_3 == 1
+    assert s_4 == 1 and c_4 == 0
+    assert s_5 == 0 and c_5 == 1
